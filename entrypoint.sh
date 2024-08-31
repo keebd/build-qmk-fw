aaa43558e42cec051516b6340820aa9ddfcf467f
medoix <medoix@shivv.com>
2024-09-01 08:52:03 +1000

    Add support for local entire keyboard configs that do not exist in QMK yet

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,25 @@ readonly keyboard="$1"
 readonly keymap="$2"
 readonly controller="$3"
 readonly qmk_output="$4"
-readonly local_keymap="$5"
+readonly local_keyboard="$5"
+readonly local_keymap="$6"
+
+# Used if this keyboard config does not exist in upstream QMK Firmware
+if [ -n "$local_keyboard" ]; then
+  keyboard_lookup_dir="/opt/qmk_firmware/keyboards"
+
+  until find "$keyboard_lookup_dir" -type d -name $keyboard | grep -q .; do
+    keyboard_lookup_dir=$(dirname "$keyboard_lookup_dir")
+  done
+
+  if [ "$keyboard_lookup_dir" = "/opt/qmk_firmware/keyboards/$keyboard" ]; then
+    echo "Keyboard $keyboard exists in QMK Firmware, use `local-keymap-path` instead."
+    exit 1
+  fi
+
+  echo "Copying local keyboard into $keyboard_lookup_dir"
+  cp -rv "$local_keyboard" "$keyboard_lookup_dir/$(basename "$local_keyboard")"
+fi
 
 # Find the keymaps directory the same way QMK CLI does
 if [ -n "$local_keymap" ]; then
